e6206f5ec62830c053702bace05738c8f6dd92da
rodrigo <rodrigotanure4@gmail.com>
2023-02-04 12:44:26 -0300

    alteração

--- scripts.js
+++ scripts.js
@@ -1,12 +1,50 @@
+$(window).scrollTop(0);
+
 $(window).scroll(function () {
-        
-    if ($(window).scrollTop() > 145) {
-        $('.relogios').addClass('acender');
-        $('.relogios').removeClass('invisible');
+    
+    
+    if ($(window).scrollTop() == 0) {
+        // $('.relogios').addClass('apagar');
+        // $('.relogios').removeClass('invisible');        
+        // $('#nossos-relogios').addClass('apagar');
+        // $('#nossos-relogios').removeClass('invisible');        
+    }
+    
+    if ($("#nossos-relogios").offset().top - $(window).scrollTop() < 200) {
+        $('#nossos-relogios').removeClass('invisible');
+        $('#nossos-relogios').removeClass('apagar');
+        $('#nossos-relogios').addClass('acender');
+    } 
+    if($("#nossos-relogios").offset().top - $(window).scrollTop() > 200) {
+        $('#nossos-relogios').removeClass('acender');
+        $('#nossos-relogios').addClass('apagar');
+
+    }
+
+    if ($("#clocks").offset().top - $(window).scrollTop() < 500) {
+        $('#clocks').removeClass('invisible');
+        $('#clocks').removeClass('apagar');
+        $('#clocks').addClass('acender');
+    }
+    if ($("#clocks").offset().top - $(window).scrollTop() > 500) {
+        $('#clocks').removeClass('acender');
+        $('#clocks').addClass('apagar');
     }
 });
 
-$(window).scrollTop(0);
+console.log($('#clocks').offset().top - $(window).scrollTop())
+
+console.log($('#nossos-relogios').scrollTop());
+
+// $(window).on("scroll", function(){
+//     console.log($('#clocks').offset().top - $(window).scrollTop());
+//     if ($("#clocks").offset().top - $(window).scrollTop() < 500) {
+//         $('#clocks').removeClass('invisible');
+//         $('#clocks').addClass('acender');
+//         console.log($("#clocks").offset().top - $(window).scrollTop());
+//         console.log("oi")
+//     }
+// });
 
 
 /*
